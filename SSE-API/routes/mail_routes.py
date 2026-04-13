9454942204bbfe4576b97fdb0c476504331a4751
SammI Blackburn <32447824+sammysam1198@users.noreply.github.com>
2026-04-12 21:05:15 -0400

    test mailer

--- SSE-API/routes/mail_routes.py
+++ SSE-API/routes/mail_routes.py
@@ -7,7 +7,7 @@ test_bp = Blueprint("test", __name__)
 @test_bp.get("/test-email")
 def test_email():
     send_email(
-        "yourrealemail@gmail.com",
+        "maidenfann1198@gmail.com",
         "SSE Email Test",
         "<h1>It works</h1><p>If you see this, email is wired.</p>"
     )
